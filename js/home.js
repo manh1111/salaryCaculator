8de4df11a0b37f4ec145c7c410328d493db146fb
manh1111 <minhanh746347@gmail.com>
2023-05-14 17:12:33 +0700

    update

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 let infoTeacher = undefined;
 const listInput = ['teacherCode', 'name', 'dob', 'email', 'phoneNumber', 'address']
+let dataListTeacher = []
 
 // payrollModal
 const closeBtn = $('.js-close-payroll-btn')
@@ -18,8 +19,8 @@ modal.addEventListener('click', () => {
     modal.classList.remove('show-modal');
 })
 modalContainer.addEventListener('click', (event) => {
-        event.stopPropagation();
-    }
+    event.stopPropagation();
+}
 )
 
 
@@ -29,12 +30,23 @@ const createClassModal = $(".modal-create-class")
 const closeCreate = $(".js-close-create-class")
 createBtn.addEventListener('click', () => {
     createClassModal.classList.add('show-modal')
+    optionCreateClass()
 })
 closeCreate.addEventListener('click', () => {
     createClassModal.classList.remove('show-modal')
 })
 
-
+//createSubject
+const createSubBtn = $(".create-subject-btn")
+const createSubModal = $(".modal-create-subject")
+const closeCreateSub = $(".js-close-create-subject")
+createSubBtn.addEventListener('click', () => {
+    createSubModal.classList.add('show-modal')
+    
+})
+closeCreateSub.addEventListener('click', () => {
+    createSubModal.classList.remove('show-modal')
+})
 //addTeacherModal
 const showModalAdd = $(".add-teacher-btn")
 const addTeacherModal = $(".modal-add-teacher")
@@ -55,16 +67,21 @@ items.forEach((item, index) => {
     item.addEventListener('click', () => {
         contentItems.forEach((item, index) => {
             if (item.classList.contains('show-content')) {
-                item.classList.remove('show-content') 
+                item.classList.remove('show-content')
             }
         })
         showItem = contentItems[index]
-        $('.js-sider').classList.add('show-content')
+        if (index == 0 || index == 1) {
+            $('.js-sider').classList.add('show-content')
+        }
+        else {
+            $('.js-sider').classList.remove('show-content')
+        }
         showItem.classList.add('show-content')
         $('.input-id').addEventListener('click', (event) => {
             event.stopPropagation();
         })
-        if (index == 0){
+        if (index == 0) {
             i.classList.remove('hide')
         }
         else {
@@ -100,46 +117,61 @@ const showTeacher = () => {
             if (item === 'dob') {
                 input.value = infoTeacher[`${item}`].slice(0, 10);
             }
-            else{
+            else {
                 input.value = infoTeacher[`${item}`]
             }
         }
     })
     let select = $('select[name="degree"]')
-    select.value =  infoTeacher.degree
+    select.value = infoTeacher.degree
 }
 
-const apiGetTeacher = 'https://103.69.193.30.nip.io/teachers/get'
 const listTeacher = $('#list-teacher')
-function getTeacher(callback) {
+const showSider = (data) => {
+    while (listTeacher.firstChild) {
+        listTeacher.removeChild(listTeacher.firstChild); // Xóa các phần tử li cho đến khi không còn phần tử con
+    }
+    for (let i = 0; i < data.length; i++) {
+        let teacher = data[i];
+        let li = document.createElement("li");
+        li.textContent = teacher.teacherCode;
+
+        // Thêm lớp "highlight" vào thẻ li
+        li.classList.add("sider-item");
+        li.onclick = () => {
+            infoTeacher = teacher;
+            showTeacher()
+            
+        }
+        listTeacher.appendChild(li);
+    }
+}
+
+let listIDTeacher = []
+let listNameTeacher = []
+const apiGetTeacher = 'https://103.69.193.30.nip.io/teachers/get'
+function getTeacher() {
     //api lấy thông tin giáo viên 
     fetch(apiGetTeacher)
         .then((response) => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        // Read the response as json.
-        return response.json();
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            // Read the response as json.
+            return response.json();
         })
         .then((response) => {
             if (response.success) {
+                dataListTeacher = [...response.data];
                 for (let i = 0; i < response.data.length; i++){
-                    let teacher = response.data[i];
-                    let li = document.createElement("li");
-                    li.textContent = teacher.teacherCode;
-
-                    // Thêm lớp "highlight" vào thẻ li
-                    li.classList.add("sider-item");
-                    li.onclick = () => {
-                        infoTeacher = teacher
-                        callback();
-                    }  
-                    listTeacher.appendChild(li);
+                    listIDTeacher[i] = response.data[i]._id
+                    listNameTeacher[i] = response.data[i].name
                 }
+                showSider(dataListTeacher)
             }
         })
         .catch(e => {
-        console.log(e)
+            console.log(e)
         })
 }
 
@@ -147,16 +179,16 @@ const apiPostTeacher = 'https://103.69.193.30.nip.io/teachers/create'
 function createTeacher(data) {
     fetch(apiPostTeacher, {
         method: "POST",
-    //     mode: "cors", // no-cors, *cors, same-origin
-    //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    //     credentials: "include", // include, *same-origin, omit
+        //     mode: "cors", // no-cors, *cors, same-origin
+        //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        //     credentials: "include", // include, *same-origin, omit
         headers: { 'Content-Type': 'application/json' },
-    //     redirect: "follow", // manual, *follow, error
-    //     referrerPolicy: "no-referrer",
+        //     redirect: "follow", // manual, *follow, error
+        //     referrerPolicy: "no-referrer",
         body: JSON.stringify(data)
     })
         .then(function (response) {
-            return response.json();  
+            return response.json();
         })
         .then((response) => {
             //show new teacher
@@ -166,44 +198,268 @@ function createTeacher(data) {
                     if (item === 'dob') {
                         input.value = response.data[`${item}`].slice(0, 10);
                     }
-                    else{
+                    else {
                         input.value = response.data[`${item}`]
                     }
                 }
             })
-            let select = $('.show-info elect[name="degree"]')
-            select.value = response.data.degree
-            
-            li.textContent = response.data.teacherCode;
-            li.classList.add("sider-item");
+            let select = $('.show-info select[name="degree"]')
+            if (select) {
+                select.value = response.data.degree
+            }
+            dataListTeacher.push(response.data);
+            console.log(dataListTeacher);
+            showSider(dataListTeacher);
         })
         .catch(e => {
             console.log(e)
         })
 }
 
-function handleAddTeacher(){
+function handleAddTeacher() {
     const addTeacherBtn = $('.add-teacher')
     addTeacherBtn.onclick = () => {
         let info = []
         var data = {}
-        for(let item of listInput){
+        for (let item of listInput) {
             if (item != 'teacherCode') {
-                info[item] = $((`.modal-add-teacher input[name="${item}"]`)).value 
-                let degreeTeacher = $((`.modal-add-teacher select[name="degree"]`)).value 
-                data[`${item}`]= info[item]
+                info[item] = $((`.modal-add-teacher input[name="${item}"]`)).value
+                let degreeTeacher = $((`.modal-add-teacher select[name="degree"]`)).value
+                data[`${item}`] = info[item]
                 data.degree = degreeTeacher
             }
         }
-        data['cmnd'] = '26663'
+        data['cmnd'] = Math.random().toString()
         createTeacher(data)
         addTeacherModal.classList.remove('show-modal')
     }
 }
 
+let listIDClass = []
+let listNameClass = []
+const listClass = $('#list-class')
+const apiGetClass = 'https://103.69.193.30.nip.io/classes/get'
+function getClass() {
+    fetch(apiGetClass)
+        .then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then((response) => {
+            if (response.success) {
+                while (listClass.firstChild) {
+                    listClass.removeChild(listClass.firstChild)
+                }
+                for (let i = 0; i < response.data.length; i++) {
+                    let tr = document.createElement("tr");
+                    let classObject = response.data[i];
+                    let nameSub = ''
+                    if (classObject.hasOwnProperty(Subject)) {
+                        // nameSub = classObject.Subject.name
+                        console.log('123')
+                    } else {
+                        nameSub = "Đã xóa"
+                        console.log('321')
+                    }
+                    let data = [i + 1, classObject.name, nameSub, classObject.Teacher.name,
+                        classObject.studentNumber]
+                        for (let j = 0; j < 6; j++) {
+                        let td = document.createElement("td");
+                        td.textContent = data[j]
+                        if (j === 5) {
+                            let i = document.createElement('i')
+                            i.classList.add('fa-solid')
+                            i.classList.add('fa-xmark')
+                            td.appendChild(i)
+                        }
+                        tr.appendChild(td)
+                    }
+                    listClass.appendChild(tr)
+                }
+            }
+            for (let i = 0; i < response.data.length; i++){
+                    listIDClass[i] = response.data[i]._id
+                    listNameClass[i] = response.data[i].name
+            }
+        })
+        .catch(e => {
+            console.log(e)
+        })
+}
+
+function handleAddClass() {
+    const addClassBtn = $('.create-class')
+    addClassBtn.onclick = () => {
+        var data = {}
+        var key = ['name', 'name-subject', 'name-class', 'studentNumber']
+        var list = ['Teacher', 'name', 'Subject', 'studentNumber']
+        for (let i = 0; i < 4; i++) {
+            if (key[i] === 'name' || key[i] === 'name-subject') {
+                let classTeacher = $('.modal-create-class select[name="name"]').value
+                data.Teacher = classTeacher
+                let subjectName = $('.modal-create-class select[name="name-subject"]').value
+                data.Subject = subjectName
+            }             
+            data['name'] = $(`.modal-create-class input[name="name-Class"]`).value 
+            data['studentNumber'] = $(`.modal-create-class input[name="studentNumber"]`).value 
+        }
+        createClass(data)
+        createClassModal.classList.remove('show-modal')
+    }
+}
+
+function optionCreateClass() {
+    for (let i = 0; i < listIDTeacher.length; i++) {
+        let option = document.createElement("option")
+        option.classList.add('nameOption')
+        option.innerText = listNameTeacher[i]
+        option.value = listIDTeacher[i]
+        $('.modal-create-class .block-input:nth-child(1) select').appendChild(option)
+    }
+    for (let i = 0; i < listIDSubject.length; i++) {
+        let option = document.createElement("option")
+        option.classList.add('nameSubject-option')
+        option.innerText = listNameSubject[i]
+        option.value = listIDSubject[i]
+        $('.modal-create-class .block-input:nth-child(2) select').appendChild(option)
+    }
+}
+
+const apiPostClass = 'https://103.69.193.30.nip.io/classes/create'
+function createClass(data) {
+    fetch(apiPostClass, {
+        method: "POST",
+        //     mode: "cors", // no-cors, *cors, same-origin
+        //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        //     credentials: "include", // include, *same-origin, omit
+        headers: { 'Content-Type': 'application/json' },
+        //     redirect: "follow", // manual, *follow, error
+        //     referrerPolicy: "no-referrer",
+        body: JSON.stringify(data)
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(getClass())
+        .catch(e => {
+            console.log(e)
+        })
+}
+
+const apiDeleteSubject = 'https://103.69.193.30.nip.io/subject/delete'
+function handleDeleteSubject(id) {
+    fetch(apiDeleteSubject +"?id=" + id, {
+        method: "Delete",
+        //     mode: "cors", // no-cors, *cors, same-origin
+        //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        //     credentials: "include", // include, *same-origin, omit
+        headers: { 'Content-Type': 'application/json' },
+        //     redirect: "follow", // manual, *follow, error
+        //     referrerPolicy: "no-referrer",
+    })
+    .then(
+        getSubject()
+    )
+    .catch(e => {
+        console.log(e)
+    })
+}
+
+let listIDSubject = []
+let listNameSubject = []
+const listSubject = $('#list-subject')
+const apiGetSubject = 'https://103.69.193.30.nip.io/subject/get'
+function getSubject() {
+    fetch(apiGetSubject)
+        .then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then((response) => {
+            if (response.success) {
+                while (listSubject.firstChild) {
+                    listSubject.removeChild(listSubject.firstChild)
+                }
+                for (let i = 0; i < response.data.length; i++) {
+                    let tr = document.createElement("tr");
+                    let subjectObject = response.data[i];
+                    const data = [i + 1, subjectObject.name, subjectObject.subjectCode, subjectObject.subjectCoefficients,
+                        subjectObject.lession]
+                    for (let j = 0; j < 6; j++) {
+                        let td = document.createElement("td");
+                        td.textContent = data[j]
+                        if (j == 5) {
+                            let iElement = document.createElement('i')
+                            iElement.classList.add('fa-solid')
+                            iElement.classList.add('fa-xmark')
+                            iElement.onclick = ()=>{handleDeleteSubject(listIDSubject[i])}
+                            td.appendChild(iElement)
+                        }
+                        tr.appendChild(td)
+                    }
+                    listSubject.appendChild(tr)
+                }
+            }
+            for (let i = 0; i < response.data.length; i++){
+                listIDSubject[i] = response.data[i]._id
+                listNameSubject[i] = response.data[i].name
+            }
+        })
+        .catch(e => {
+            console.log(e)
+        })
+}
+
+
+function handleAddSubject() {
+    const addSubjectBtn = $('.create-subject')
+    addSubjectBtn.onclick = () => {
+        let info = []
+        var data = {}
+        var key = ['name-subject', 'subjectCode', 'sub-coefficients', 'lession']
+        var list = ['name', 'subjectCode', 'subjectCoefficients', 'lession']
+        for (let i = 0; i < 4; i++) {
+            info[list[i]] = $((`.modal-create-subject input[name="${key[i]}"]`)).value
+            data[`${list[i]}`] = info[list[i]]
+        }
+        createSubject(data)
+        createSubModal.classList.remove('show-modal')
+    }
+}
+
+const apiPostSubject = 'https://103.69.193.30.nip.io/subject/create'
+function createSubject(data) {
+    fetch(apiPostSubject, {
+        method: "POST",
+        //     mode: "cors", // no-cors, *cors, same-origin
+        //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        //     credentials: "include", // include, *same-origin, omit
+        headers: { 'Content-Type': 'application/json' },
+        //     redirect: "follow", // manual, *follow, error
+        //     referrerPolicy: "no-referrer",
+        body: JSON.stringify(data)
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(getSubject())
+        .catch(e => {
+            console.log(e)
+        })
+}
+
+
 function run() {
     getTeacher(showTeacher);
     handleAddTeacher()
+    getClass()
+    getSubject()
+    handleAddClass()
+    handleAddSubject()
 }
 
 run();
